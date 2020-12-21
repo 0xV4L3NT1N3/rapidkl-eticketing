1c3a17ffa2a5bf90be1405cf2ecf506468bdfc4d
zzSleepyCoderzz <timothycgy6@gmail.com>
2020-12-21 22:32:25 +0800

    Home Page

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -64,8 +64,56 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('hello'),
+    return Scaffold(
+      backgroundColor: Colors.grey[50],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Page title
+          Padding(
+            padding: const EdgeInsets.only(top: 30.0, left: 15.0),
+            child: Text(
+              'Book a Ticket',
+              style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueGrey[800]),
+            ),
+          ),
+          // Location/destination card
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+            child: Container(
+              child: Card(
+                child: Stack(
+                  children: [
+                    Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.assistant_photo),
+                              border: InputBorder.none,
+                              hintText: 'Your location'),
+                        ),
+                        Divider(),
+                        TextField(
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.location_on_rounded),
+                              border: InputBorder.none,
+                              hintText: 'Destination station'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
