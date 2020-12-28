e9681744368c4c51ca22b4bf450687d3658d92d5
zzSleepyCoderzz <timothycgy6@gmail.com>
2020-12-28 20:00:33 +0800

    Part Progress

--- lib/Pages/News.dart
+++ lib/Pages/News.dart
@@ -2,18 +2,82 @@ import 'package:flutter/material.dart';
 import 'package:rapidkl/Services/User.dart';
 import 'package:rapidkl/Services/database.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class News extends StatefulWidget {
   @override
+
   _NewsState createState() => _NewsState();
 }
 
-class _NewsState extends State<News> {
-  final List<String> items = ["hello", "world", "lmao"];
+class _NewsState extends State<News> with SingleTickerProviderStateMixin {
+
+  //animated icon
+  AnimationController _animationController;
+  Animation _colorAnimation;
+  Animation <double> _sizeAnimation;
+  Animation _curve;
+  IconData icon = Icons.notifications_on;
+  int count = 0;
 
   @override
+
+  setInt(int lol) async{
+    int count = 0;
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('counter' , count);
+  }
+
+  getInt() async{
+    final prefs = await SharedPreferences.getInstance();
+    final counter = prefs.getInt('counter') ?? 0;
+    return counter;
+  }
+
+
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      duration: Duration(milliseconds: 200),
+      vsync: this,
+    );
+
+    _curve = CurvedAnimation(parent: _animationController, curve: Curves.easeInCubic);
+
+    _colorAnimation = ColorTween(begin: Colors.red, end: Colors.grey)
+        .animate(_curve);
+
+    _sizeAnimation = TweenSequence(
+        <TweenSequenceItem<double>>[
+          TweenSequenceItem<double>(
+            tween: Tween<double> (begin: 30, end: 35),
+            weight: 50,
+
+          ),
+          TweenSequenceItem<double>(
+            tween: Tween<double> (begin: 35, end: 30),
+            weight: 50,
+          ),
+        ]
+    ).animate(_curve);
+
+    @override
+    void dispose() {
+      super.dispose();
+      _animationController.dispose();
+    }
+
+
+
+
+
+
+  }
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
+    Tween<Offset> _offset = Tween(begin: Offset(1, 0), end: Offset(0, 0));
+    final GlobalKey <AnimatedListState> _listKey = GlobalKey <AnimatedListState>();
 
     return Scaffold(
       body: Column(children: [
@@ -36,10 +100,31 @@ class _NewsState extends State<News> {
             // Notification bell
             Padding(
               padding: const EdgeInsets.only(top: 50.0, left: 210),
-              child: IconButton(
-                  icon: Icon(Icons.notifications),
-                  color: Colors.red,
-                  onPressed: () {}),
+              child: AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (BuildContext context, _) {
+                    return IconButton(
+                      icon: Icon( icon,
+                        color: _colorAnimation.value,
+                        size: _sizeAnimation.value,
+                      ),
+                      onPressed: () {
+                        if (count == 0){
+                          _animationController.forward();
+                          count++;
+                          icon = Icons.notifications_off;
+                          setInt(count);
+                        }
+                        else{
+                          _animationController.reverse();
+                          count =0;
+                          icon = Icons.notifications_on;
+                          setInt(count);
+                        }
+                      },
+                    );
+                  }
+              ),
             )
           ],
         ),
@@ -52,70 +137,74 @@ class _NewsState extends State<News> {
             NewsFunc newslol2 = snapshot.data;
             if (snapshot.hasData) {
               var arr = newslol2.news;
-              return Expanded(
-                  child: ListView.builder(
-                      itemCount: arr.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          height: 120,
-                          child: Card(
-                            margin:
-                                EdgeInsets.only(left: 10, right: 10, bottom: 5),
-                            elevation: 2,
-                            // Prevents picture from overriding card borders
-                            clipBehavior: Clip.antiAliasWithSaveLayer,
-                            borderOnForeground: true,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15.0),
-                            ),
-                            child: Stack(
-                              children: <Widget>[
-                                Container(
+              return  Expanded(
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: arr.length,
+                            itemBuilder: (context, index) {
+                              return GestureDetector(
+                                child: Container(
                                   height: 120,
-                                  width: 120,
-                                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(15))),
-                                  child: Image.network(
-                                      'https://apicms.thestar.com.my/uploads/images/2020/06/06/709535.jpg',
-                                      fit: BoxFit.fill),
-                                ),
-                                Positioned(
-                                  left: 130,
-                                  top: 15,
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        'Get the new MY30 ',
-                                        overflow: TextOverflow.ellipsis,
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.blueGrey[800],
-                                            fontSize: 22),
-                                      ),
-                                      Text(
-                                          'Available for a limited time \n only at selected stations',
-                                          style: TextStyle(
-                                              color: Colors.grey,
-                                              fontSize: 15)),
-                                      Padding(
-                                        padding: const EdgeInsets.only(
-                                            left: 110.0, top: 10),
-                                        child: Text('2 hours ago',
-                                            style:
-                                                TextStyle(color: Colors.grey)),
-                                      ),
-                                    ],
+                                  child: Card(
+                                    margin:
+                                    EdgeInsets.only(left: 10, right: 10, bottom: 5),
+                                    elevation: 2,
+                                    // Prevents picture from overriding card borders
+                                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                                    borderOnForeground: true,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15.0),
+                                    ),
+                                    child: Stack(
+                                      children: <Widget>[
+                                        Container(
+                                          height: 120,
+                                          width: 120,
+                                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                                          decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(15))),
+                                          child: Image.network(
+                                              'https://firebasestorage.googleapis.com/v0/b/rapidkl-app.appspot.com/o/NewsPics%2Fmrt-corp_20171009170253_theedgemarkets.jpg?alt=media&token=1be05923-e830-4da6-98ea-697305ce2d54',
+                                              fit: BoxFit.fill),
+                                        ),
+                                        Positioned(
+                                          left: 130,
+                                          top: 15,
+                                          child: Column(
+                                            crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                arr[index],
+                                                overflow: TextOverflow.ellipsis,
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.blueGrey[800],
+                                                    fontSize: 22),
+                                              ),
+                                              Text(
+                                                  'Available for a limited time \n only at selected stations',
+                                                  style: TextStyle(
+                                                      color: Colors.grey,
+                                                      fontSize: 15)),
+                                              Padding(
+                                                padding: const EdgeInsets.only(
+                                                    left: 110.0, top: 10),
+                                                child: Text('2 hours ago',
+                                                    style:
+                                                    TextStyle(color: Colors.grey)),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
-                              ],
-                            ),
-                          ),
-                        );
-                      }));
+                              );
+                            }),
+                      );
             } else {
               return Container(
                 height: 10,
