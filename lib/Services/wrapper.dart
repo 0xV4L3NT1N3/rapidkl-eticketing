a3d4504c456a95d97f58e9c19bb87e3cd638fe34
zzSleepyCoderzz <timothycgy6@gmail.com>
2020-12-31 02:55:12 +0800

    Remove Auth

--- lib/Services/wrapper.dart
+++ lib/Services/wrapper.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rapidkl/Pages/Home.dart';
-import 'package:rapidkl/Services/Authenticate.dart';
+import 'package:rapidkl/Pages/signin.dart';
 import 'package:provider/provider.dart';
 import 'package:rapidkl/Services/User.dart';
 
@@ -13,7 +13,7 @@ class  Wrapper extends StatelessWidget {
     //check to see if user is logged in or not//
 
     if(user == null){
-      return Auth();
+      return SignIn();
     }
     else {
       return MyApp();
