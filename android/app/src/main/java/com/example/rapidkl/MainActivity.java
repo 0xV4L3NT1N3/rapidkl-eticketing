320a41fb5896e27eb0d95f1b30e21f4d2eaffc67
zzSleepyCoderzz <timothycgy6@gmail.com>
2020-12-31 16:59:13 +0800

    Update MainActivity.java

--- android/app/src/main/java/com/example/rapidkl/MainActivity.java
+++ android/app/src/main/java/com/example/rapidkl/MainActivity.java
@@ -3,5 +3,4 @@ package com.example.rapidkl;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-    GeneratedPluginRegistrant.registerWith(this);
 }
